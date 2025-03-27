b83e0f4be5f915278a336706e613072adaecfa72
Mitesh Kumar <miteshkumar82115@gmail.com>
2025-03-27 22:40:05 +0530

    Update 011Binary_searchQuestions.cpp

--- 011Binary_searchQuestions.cpp
+++ 011Binary_searchQuestions.cpp
@@ -127,6 +127,57 @@ long double morePrecise(int n,int precision,int tempSol){
 
 }
 
+/*question 5*/
+//BOOK ALLOCATION PROBLEM
+bool isPossible(int arr[], int n, int m, int mid) {
+    int studentCount=1;
+    int pageSum=0;
+  
+
+    for(int i=0;i<n;i++){
+        if(pageSum+arr[i]<=mid){
+            pageSum+=arr[i];
+        }
+        else{
+            studentCount++;
+            if(studentCount>m||arr[i]>mid){
+                return false;
+            }
+            pageSum=arr[i];
+        }
+    }
+    return true;
+}
+
+
+
+int findPages(int arr[], int n, int m) {
+    int s=0;
+    int sum=0;
+
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    int e=sum;
+    int ans=-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(isPossible(arr,n,m,mid)){
+            ans=mid;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+    if(n<m){
+        ans=-1;
+    }
+    return ans;
+
+}
+
 
 
 
@@ -155,6 +206,13 @@ int main(){
    cin>>n;
    int tempSol=binarySearch(n);
    cout<<"Answewr is "<<morePrecise(n,5,tempSol)<<endl;
+
+    //question5
+   int arr[]={25, 46, 28, 49, 24};
+   int n=5;
+   int m=4;
+   int result = findPages(arr, n, m);
+   cout << "Minimum number of pages: " << result << endl;
    
 
    return 0;
