f4d55e720b06395213b947355f67a436bc92521a
Mitesh Kumar <miteshkumar82115@gmail.com>
2025-04-28 16:41:37 +0530

    Update 025Recursion.cpp

--- 025Recursion.cpp
+++ 025Recursion.cpp
@@ -93,7 +93,63 @@ void printDigits(int n, string arr[]){
     printDigits(n,arr);
     cout<<arr[digit]<<" ";
 }
+bool isSorted(int *arr,int size){
+    if(size==0||size==1){
+        return true;
+    }
+    if(arr[0]>arr[1]){
+        return false;
+    }
+    else{
+        return isSorted(arr+1,size-1);
+    }
+}
 
+int sumofarray(int *arr,int size){
+    int sum=0;
+
+    if(size==0){
+        return 0;
+    }
+    
+    sum=arr[0]+sumofarray(arr+1,size-1);
+    return sum;
+}
+
+bool isFound(int *arr,int size,int key){
+    if(size==0){
+        return 0;
+    }
+    if(arr[0]==key){
+        return true;
+    }
+    else{
+    
+    isFound(arr+1,size-1, key);
+
+    }   
+    
+    
+}
+
+bool binarySearch(int *arr,int s,int e,int key){
+    if(s>e){
+        return false;
+    }
+    int mid=s+(e-s)/2;
+    if(arr[mid]==key){
+        return true;
+    }
+    
+    if(arr[mid]<key){
+        return binarySearch(arr,mid+1,e,key);
+    }
+    else{
+        return binarySearch(arr,s,mid-1,key);
+    }
+    
+}
+    
 
 int main(){
 
@@ -123,6 +179,7 @@ int main(){
     // int ans=fib(n);
     // cout<<ans<<endl;
 
+//Stairs Problem
     // int n;
     // cin>>n;
     // cout<<endl;
@@ -130,13 +187,40 @@ int main(){
     // cout<<ans<<endl;
 
 //Print digits
-    string arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
-    int n;
-    cin>>n;
-    cout<<endl;
-    printDigits(n,arr);
-    cout<<endl;
+    // string arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    // int n;
+    // cin>>n;
+    // cout<<endl;
+    // printDigits(n,arr);
+    // cout<<endl;
 
+
+//IsSorted
+    int arr[7]={4,5,5,5,6,7,8};
+    int size=7;
+    // bool ans=isSorted(arr,size);
+    // if(ans){
+    //     cout<<"Array is sorted"<<endl;
+    // }
+    // else{
+    //     cout<<"Array is not sorted"<<endl;
+    // }
+
+//Sum by recusion
+    // int ans=sumofarray(arr,size);
+    // cout<<ans<<endl;
+
+
+//element searching
+    //cout<<isFound(arr,size,6)<<endl;
+
+
+
+//Binary search
+    int s=0;
+    int e=size-1;
+    int key=5;
+    cout<<binarySearch(arr,s,e,key)<<endl;
 
 
 
