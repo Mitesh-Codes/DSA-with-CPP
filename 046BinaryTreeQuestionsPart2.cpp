31a0e671f6cf58d81181fca84388bc015bd6a888
Mitesh Kumar <miteshkumar82115@gmail.com>
2025-09-05 12:20:46 +0530

    Update 046BinaryTreeQuestionsPart2.cpp

--- 046BinaryTreeQuestionsPart2.cpp
+++ 046BinaryTreeQuestionsPart2.cpp
@@ -246,20 +246,91 @@ void printLevelOrder(Node* root) {
 
 
 //Question21: Brurning Tree
+Node* createParentMapping(Node* root,int target,map<Node*,Node*>&nodeToParent){
+        Node* res=NULL;
+        queue<Node*>q;
+        q.push(root);
+        nodeToParent[root]=NULL;
+        while(!q.empty()){
+            Node*front=q.front();
+            q.pop();
+            if(front->data==target){
+                res=front;
+            }
+            if(front->left){
+                nodeToParent[front->left]=front;
+                q.push(front->left);
+            }
+            if(front->right){
+                nodeToParent[front->right]=front;
+                q.push(front->right);
+            }
+        }
+        return res;
+}
+int burnTree(Node* root,int target,map<Node*,Node*>&nodeToParent){
+        map<Node*,bool> visited;
+        queue<Node*>q;
+        q.push(root);
+        visited[root]=true;
+        int ans=0;
+        
+        while(!q.empty()){
+            bool flag=0;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Node*front=q.front();
+                q.pop();
+                if(front->left && !visited[front->left]){
+                    flag=1;
+                    q.push(front->left);
+                    visited[front->left]=1;
+                }
+                if(front->right && !visited[front->right]){
+                    flag=1;
+                    q.push(front->right);
+                    visited[front->right]=1;
+                }
+                if(nodeToParent[front] && !visited[nodeToParent[front]]){
+                    flag=1;
+                    q.push(nodeToParent[front]);
+                    visited[nodeToParent[front]]=1;
+                }
+            }
+            if(flag==1){
+                ans++;
+            }
+        }
+        return ans;
+}
+int minTime(Node* root, int target) {
+        // code here
+        map<Node*,Node*>nodeToParent;
+        Node* targetNode=createParentMapping(root,target,nodeToParent);
+        int ans=burnTree(targetNode,target,nodeToParent);
+        return ans;
+}
 
 
+//Morris Traversal (TC: O(n), SC: O(1))
+
+
+
+
+
+//Question22: Flatten Binary Tree To Linked List
 
 
 
 int main(){
-    // Node *root = new Node(4);
-    // root->left = new Node(2);
-    // root->right = new Node(5);
-    // root->left->left = new Node(7);
-    // root->left->right = new Node(1);
-    // root->right->right = new Node(6);
-    // root->left->right->left = new Node(3);
-    // root->left->right->right = new Node(8);
+    Node *root = new Node(4);
+    root->left = new Node(2);
+    root->right = new Node(5);
+    root->left->left = new Node(7);
+    root->left->right = new Node(1);
+    root->right->right = new Node(6);
+    root->left->right->left = new Node(3);
+    root->left->right->right = new Node(8);
 
 //Question14: Sum of Longest Bloodline of a Tree
     
@@ -293,17 +364,28 @@ int main(){
 
 
 //Question20: Create a Binary Tree from Inorder and Postorder
-    vector<int> inorder  = {4, 2, 5, 1, 3};
-    vector<int> postorder = {4, 5, 2, 3, 1};
+    // vector<int> inorder  = {4, 2, 5, 1, 3};
+    // vector<int> postorder = {4, 5, 2, 3, 1};
 
-    Node* root = buildTree2(inorder, postorder);
+    // Node* root = buildTree2(inorder, postorder);
 
-    cout << "Level Order Traversal of constructed tree: ";
-    printLevelOrder(root);
-    cout << endl;
+    // cout << "Level Order Traversal of constructed tree: ";
+    // printLevelOrder(root);
+    // cout << endl;
 
 
 //Question 21: Burning Tree
+    //cout<<minTime(root,7)<<endl;
+
+
+
+
+//Morris Traversal
+
+
+
+//Question22: Flatten Binary Tree To Linked List
+
 
 
     
